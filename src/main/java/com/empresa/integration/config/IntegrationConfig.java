8de3618eead3b7b89708b45ec91722aeb30c82d2
nbaez001 <nbaez001@gmail.com>
2025-01-06 17:23:37 -0500

    Version3

--- src/main/java/com/empresa/integration/config/IntegrationConfig.java
+++ src/main/java/com/empresa/integration/config/IntegrationConfig.java
@@ -11,7 +11,13 @@ import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.json.JsonToObjectTransformer;
 import org.springframework.integration.json.ObjectToJsonTransformer;
 import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
+import org.springframework.integration.transformer.HeaderEnricher;
+import org.springframework.integration.transformer.support.HeaderValueMessageProcessor;
+import org.springframework.integration.transformer.support.StaticHeaderValueMessageProcessor;
 import org.springframework.messaging.MessageChannel;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableIntegration
@@ -29,19 +35,31 @@ public class IntegrationConfig {
     }
 
     @Bean
+    @Transformer(inputChannel = "integration.student.gateway.channel",
+            outputChannel = "integration.student.toConvertObject.channel")
+    public HeaderEnricher enricherHeader() {
+        Map<String, HeaderValueMessageProcessor<String>> headersToAdd = new HashMap<>();
+        headersToAdd.put("header1", new StaticHeaderValueMessageProcessor<>("Test Header 1"));
+        headersToAdd.put("header2", new StaticHeaderValueMessageProcessor<>("Test Header 2"));
+        return new HeaderEnricher(headersToAdd);
+    }
+
+    @Bean
     public Jackson2JsonObjectMapper getMapper() {
         ObjectMapper mapper = new ObjectMapper();
         return new Jackson2JsonObjectMapper(mapper);
     }
 
     @Bean
-    @Transformer(inputChannel = "integration.student.gateway.channel", outputChannel = "integration.student.objectToJson.channel")
+    @Transformer(inputChannel = "integration.student.toConvertObject.channel",
+            outputChannel = "integration.student.objectToJson.channel")
     public ObjectToJsonTransformer objectToJsonTransformer() {
         return new ObjectToJsonTransformer(getMapper());
     }
 
     @Bean
-    @Transformer(inputChannel = "integration.student.jsonToObject.channel", outputChannel = "integration.student.jsonToObject.fromTransformer.channel")
+    @Transformer(inputChannel = "integration.student.jsonToObject.channel",
+            outputChannel = "integration.student.jsonToObject.fromTransformer.channel")
     JsonToObjectTransformer jsonToObjectTransformer() {
         return new JsonToObjectTransformer(Student.class);
     }
