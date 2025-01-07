e305ac18ee571e5999e5f655771e460318f8eaca
nbaez001 <nbaez001@gmail.com>
2025-01-07 15:43:03 -0500

    Filters

--- src/main/java/com/empresa/integration/config/IntegrationConfig.java
+++ src/main/java/com/empresa/integration/config/IntegrationConfig.java
@@ -1,19 +1,21 @@
 package com.empresa.integration.config;
 
-import com.empresa.integration.model.Address;
 import com.empresa.integration.model.Student;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.Filter;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.MessageSelector;
+import org.springframework.integration.filter.MessageFilter;
 import org.springframework.integration.router.HeaderValueRouter;
-import org.springframework.integration.router.PayloadTypeRouter;
 import org.springframework.integration.transformer.HeaderEnricher;
 import org.springframework.integration.transformer.support.HeaderValueMessageProcessor;
 import org.springframework.integration.transformer.support.StaticHeaderValueMessageProcessor;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 
 import java.util.HashMap;
@@ -34,39 +36,16 @@ public class IntegrationConfig {
         return new DirectChannel();
     }
 
-    @ServiceActivator(inputChannel = "router.channel")
+    @Filter(inputChannel = "router.channel")
     @Bean
-    public PayloadTypeRouter router() {
-        PayloadTypeRouter router = new PayloadTypeRouter();
-        router.setChannelMapping(Student.class.getName(), "router.channel.student");
-        router.setChannelMapping(Address.class.getName(), "router.channel.address");
-        return router;
-    }
-
-    @Bean
-    @Transformer(inputChannel = "router.channel.student",
-            outputChannel = "router.channel.header")
-    public HeaderEnricher enrichHeaderForStudent() {
-        Map<String, HeaderValueMessageProcessor<String>> headersToAdd = new HashMap<>();
-        headersToAdd.put("testHeader", new StaticHeaderValueMessageProcessor<>("student"));
-        return new HeaderEnricher(headersToAdd);
-    }
-
-    @Bean
-    @Transformer(inputChannel = "router.channel.address",
-            outputChannel = "router.channel.header")
-    public HeaderEnricher enrichHeaderForAddress() {
-        Map<String, HeaderValueMessageProcessor<String>> headersToAdd = new HashMap<>();
-        headersToAdd.put("testHeader", new StaticHeaderValueMessageProcessor<>("address"));
-        return new HeaderEnricher(headersToAdd);
-    }
-
-    @ServiceActivator(inputChannel = "router.channel.header")
-    @Bean
-    public HeaderValueRouter headerRouter() {
-        HeaderValueRouter router = new HeaderValueRouter("testHeader");
-        router.setChannelMapping("student", "channel.student");
-        router.setChannelMapping("address", "channel.address");
-        return router;
+    public MessageFilter filter() {
+        MessageFilter filter = new MessageFilter(new MessageSelector() {
+            @Override
+            public boolean accept(Message<?> message) {
+                return message.getPayload() instanceof Student;
+            }
+        });
+        filter.setOutputChannelName("channel.student");
+        return filter;
     }
 }
